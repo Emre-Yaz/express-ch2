5e2cee67ae7161cbfffceaabb5a126adc301967b
Emre-Yaz <ibrahimemreyaz@gmail.com>
2023-10-18 21:43:39 +0300

    Always wear your helmet!

--- appUse.js
+++ appUse.js
@@ -1,1 +1,20 @@
-test;
+const express = require("express");
+const app = express();
+
+function validateUser(req, res, next) {
+  res.locals.validated = true;
+  console.log("validated ran!");
+  next();
+}
+
+app.use("/admin", validateUser);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/admin", (req, res) => {
+  res.send("Hello Admin!");
+});
+
+app.listen(5000, () => console.log("Server ready"));
