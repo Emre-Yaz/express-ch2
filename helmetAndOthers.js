484e2f6a2b4b43742f425331d4576c5eb9e91f93
Emre-Yaz <ibrahimemreyaz@gmail.com>
2023-10-20 19:19:03 +0300

    201 continues

--- helmetAndOthers.js
+++ helmetAndOthers.js
@@ -10,7 +10,22 @@ app.use(express.urlencoded({ extended: false }));
 
 app.post("/ajax", (req, res) => {
   console.log(req.body);
-  res.send("Test");
+  res.json("Test");
 });
+
+//
+const crypto = require("crypto");
+const nonce = crypto.randomBytes(16).toString("base64");
+console.log(nonce);
+//
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "script-src 'self' '++Tu6aQjv0ICnaN/W2CHHQ=='"
+  );
+  next();
+});
+//
 
 app.listen(5000);
