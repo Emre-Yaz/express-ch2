108f5dc32cb00c933e21a5c13989703874c55935
Emre-Yaz <ibrahimemreyaz@gmail.com>
2023-11-20 23:36:50 +0300

    keeping going

---
+++ hbsPractice.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+const helmet = require("helmet");
+app.use(helmet());
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.set("views", "views");
+app.set("view engine", "hbs");
+
+function validateUser(req, res, next) {
+  res.locals.validated = true;
+  next();
+}
+
+app.use(validateUser);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    countries: [
+      {
+        name: "Russia",
+        capital: "Moscow",
+        western: false,
+      },
+      {
+        name: "USA",
+        capital: "Washington",
+        western: true,
+      },
+    ],
+    msg: "Success!",
+    msg2: "Success2!",
+    html: "<p>img src=''</p>",
+  });
+});
+
+app.listen(5008);
